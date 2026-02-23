a2e6b34e4c059552596a0a44cd7765b860c5b0da
Faizan Ahmad <fiaznhmd.0624@gmail.com>
2026-02-23 15:20:53 +0530

    enums

---
+++ src/enum.ts
@@ -0,0 +1,21 @@
+enum Direction {
+  UP = 8,
+  DOWN,
+  LEFT,
+  RIGHT,
+}
+
+let dir: Direction = Direction.DOWN;
+// automatically get a value hover in down u will see the value if we write a value 8 in up then the value of down is 9
+
+//enum string
+
+enum Status {
+  Pending = 'PENDING',
+  Approved = 'APPROVED',
+  Rejected = 'REJECTED',
+}
+
+let stat: Status = Status.Approved;
+
+// only value we will get add on the enum
